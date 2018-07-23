98330925cb555ca5eb29904c1647480b8e8989dd
anthony abednego <paperchukz@gmail.com>
2018-07-23 16:34:37 +0700

    title dirubah

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
+	<title>ini menambahkan title</title>
 	<link rel="stylesheet" href="style.css"> <!-- ini file yang saya rubah -->
 </head>
 <body>
