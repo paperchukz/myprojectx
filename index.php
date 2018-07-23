940cd763eb2e2b18e9ea403f0ef11f9049533b74
paperchukz <paperchukz@gmail.com>
2018-07-23 16:32:16 +0700

    ini file baru

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php 
-//ini komentar
-	$y = 10;
-	echo $y;
- ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+</body>
+</html>
